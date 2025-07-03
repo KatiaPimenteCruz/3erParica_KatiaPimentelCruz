769a429d3558f462550fca4fe2b10f6db1c6a017
Katia Pimentel Cruz <katiapimentelcruz470@gmail.com>
2025-07-03 09:38:31 -0400

    Este es mi 3er Parcial de SIS-313

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './paginas/inicio/inicio';
+import { Producto } from './paginas/producto/producto';
+import { Registro } from './paginas/registro/registro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'inicial', component:Inicio },
+    {path: 'producto', component: Producto},
+    {path:'registro', component: Registro },
+];
---
+++ src/app/componentes/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterModule], //siempre poner este comando no olvidarseeeeee
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+
+}
